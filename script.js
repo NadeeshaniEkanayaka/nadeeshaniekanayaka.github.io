9879f98f05d822aac81368a78773b3de404b4a58
NadeeshaniEkanayaka <161614509+NadeeshaniEkanayaka@users.noreply.github.com>
2025-08-28 00:16:32 +0530

    script seccond commit

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
   
+  // resume.js
+
+function downloadResume() {
+  // ✅ Option 1: if you rename the file without spaces
+  const fileUrl = "files/Nadeeshani_Ekanayaka.pdf"; 
+
+  // ❌ If you keep the space, then use:
+  // const fileUrl = "files/NADEESHANI%20EKANYAKA.PDF"; 
+
+  const fileName = "NADEESHANI EKANAYAKA CV.pdf"; // name for download
+
+  const link = document.createElement("a");
+  link.href = fileUrl;
+  link.download = fileName;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
